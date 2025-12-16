920b7dafe10e4af7ac30f17f7305a3c5f551c082
Pedro Paixão <pedrohenriqueeekkj@gmail.com>
2025-12-16 10:46:10 -0300

    Update somar-impares.py

--- somar-impares.py
+++ somar-impares.py
@@ -1,26 +1,41 @@
-# Status: Não concluído
-numeros = []
-numero = 0
-soma = 0
+import os
+
+LOOP_MAXIMO = 3
     
-def mostrar_titulo():
+def mostrar_titulo(loop_maximo):
     print('Bem-vindo(a) ao programa!')
     print('-' * 60)
-    print('Contador de números ímpares\n')
+    print(f'Contador de {loop_maximo} números ímpares\n')
     
 
-def ler_numeros():
-    for i in range(1, 4, 1):
-        numero = int(input(f'{i}) Digite um número: '))
-        numeros.append(numero)
+def ler_numeros(loop_maximo):
+    numeros = []
+    for i in range(1, loop_maximo + 1):
+        while True:
+            try:
+                numero = int(input(f'{i}) Digite um número: '))
+                numeros.append(numero)
+                break
+            except ValueError:
+                print('Digite um valor inteiro!')
+    return numeros
 
-def somar_impares(soma):
+def somar_impares(numeros):
+    soma = 0
     for numero in numeros:
-        if numero % 2 == 1:
+        if numero % 2 != 0:
             soma += numero
-    print(soma)
+    return soma
 
+def exibir_resultado(soma):
+    os.system('cls')
+    print(f'A soma total dos números ímpares: {soma}')
 
-mostrar_titulo()
-ler_numeros()
-somar_impares(soma)
+def main():
+    mostrar_titulo(LOOP_MAXIMO)
+    numeros = ler_numeros(LOOP_MAXIMO)
+    soma = somar_impares(numeros)
+    exibir_resultado(soma)
+    
+if __name__ == '__main__':
+    main()
